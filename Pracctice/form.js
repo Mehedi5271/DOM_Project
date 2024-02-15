f8499bbeb51d497234c5279f57051a0d198d095e
Mehedi5271 <mhassan201205@bscse.uiu.ac.bd>
2024-02-15 23:54:35 +0600

    form

---
+++ Pracctice/form.js
@@ -0,0 +1,10 @@
+function validateForm(){
+    const form = document.forms["myForm"];
+     const value = form['fname'].value;
+
+     if(value == '')
+{
+    alert('data bosao')
+    return false
+}
+}
